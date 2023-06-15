c57c653490e59feb55fbc3a824e01eb62baf4468
Eugene Flesselle <eugene@flesselle.net>
2023-06-15 10:37:30 +0200

    update test with derivation of migration

--- tests/run/typeclass-derivation-defaultArgument.scala
+++ tests/run/typeclass-derivation-defaultArgument.scala
@@ -1,9 +1,10 @@
+import scala.deriving.Mirror as M
 import scala.deriving.*
 import scala.Tuple.*
 import scala.compiletime.*
 import scala.compiletime.ops.int.S
 
-trait Migration[From, To]:
+trait Migration[-From, +To]:
   def apply(x: From): To
 
 object Migration:
@@ -14,61 +15,55 @@ object Migration:
   given[T]: Migration[T, T] with
     override def apply(x: T): T = x
 
-  private type IndexOf[Elems <: Tuple, X] <: Int = Elems match {
-    case (elem *: elems) =>
-      elem match {
-        case X => 0
-        case _ => S[IndexOf[elems, X]]
-      }
+  type IndexOf[Elems <: Tuple, X] <: Int = Elems match {
+    case (X *: elems) => 0
+    case (_ *: elems) => S[IndexOf[elems, X]]
     case EmptyTuple => Nothing
   }
 
-  private inline def migrate[F,T](x: F): T = summonFrom {
-    case migration: Migration[F,T] => migration(x)
-  }
+  inline def migrateElem[F,T, ToIdx <: Int](from: M.ProductOf[F], to: M.ProductOf[T])(x: Product): Any =
 
-  private inline def migrateElem[F,T, ToIdx <: Int]
-                                (from: Mirror.ProductOf[F], to: Mirror.ProductOf[T])
-                                (x: Product): Any =
     type Label = Elem[to.MirroredElemLabels, ToIdx]
     type FromIdx = IndexOf[from.MirroredElemLabels, Label]
     inline constValueOpt[FromIdx] match
+
       case Some(fromIdx) =>
         type FromType = Elem[from.MirroredElemTypes, FromIdx]
         type ToType = Elem[to.MirroredElemTypes, ToIdx]
-        val elem = x.productElement(fromIdx).asInstanceOf[FromType]
-        migrate[FromType, ToType](elem)
+        summonFrom { case _: Migration[FromType, ToType] =>
+          x.productElement(fromIdx).asInstanceOf[FromType].migrateTo[ToType]
+        }
+
       case None =>
-        type HasDefault = Elem[to.MirroredElemHasDefaults, ToIdx] // NOTE when are the annotations checked ?? bug expr is not checked for match types
+        type HasDefault = Elem[to.MirroredElemHasDefaults, ToIdx]
         inline erasedValue[HasDefault] match
           case _: true => to.defaultArgument(constValue[ToIdx])
-          case _: false => compiletime.error("An element has no equivalent in source or default")
-  end migrateElem
+          case _: false => compiletime.error("An element has no equivalent or default")
 
-  private inline def migrateElems[F,T, ToIdx <: Int]
-                                 (from: Mirror.ProductOf[F], to: Mirror.ProductOf[T])
-                                 (x: Product): Seq[Any] =
+
+  inline def migrateElems[F,T, ToIdx <: Int](from: M.ProductOf[F], to: M.ProductOf[T])(x: Product): Seq[Any] =
     inline erasedValue[ToIdx] match
       case _: Tuple.Size[to.MirroredElemLabels] => Seq()
       case _ => migrateElem[F,T,ToIdx](from, to)(x) +: migrateElems[F,T,S[ToIdx]](from, to)(x)
 
-  private inline def migrateProduct[F,T](from: Mirror.ProductOf[F], to: Mirror.ProductOf[T])
-                                        (x: Product): T =
-    val elems = migrateElems[F,T,0](from, to)(x)
+  inline def migrateProduct[F,T](from: M.ProductOf[F], to: M.ProductOf[T])
+                                (x: Product): T =
+    val elems = migrateElems[F, T, 0](from, to)(x)
     to.fromProduct(new Product:
-      override def canEqual(that: Any): Boolean = false
-      override def productArity: Int = x.productArity
-      override def productElement(n: Int): Any = elems(n)
+      def canEqual(that: Any): Boolean = false
+      def productArity: Int = elems.length
+      def productElement(n: Int): Any = elems(n)
     )
 
-  implicit inline def migration[F,T](using from: Mirror.Of[F], to: Mirror.Of[T]):  Migration[F,T] =
-    new Migration[F,T]:
-      override def apply(x: F): T = inline from match
-        case fromP: Mirror.ProductOf[F] => inline to match
-          case _: Mirror.SumOf[T] => compiletime.error("Can not migrate a product to a sum")
-          case toP: Mirror.ProductOf[T] => migrateProduct[F, T](fromP, toP)(x.asInstanceOf[Product])
+  inline def migration[F,T](using from: M.Of[F], to: M.Of[T]): Migration[F,T] = (x: F) =>
+    inline from match
+      case fromP: M.ProductOf[F] => inline to match
+        case toP: M.ProductOf[T] => migrateProduct[F, T](fromP, toP)(x.asInstanceOf[Product])
+        case _: M.SumOf[T] => compiletime.error("Cannot migrate sums")
+      case _: M.SumOf[F] => compiletime.error("Cannot migrate sums")
 
 end Migration
+
 
 import Migration.*
 object Test extends App:
@@ -98,6 +93,7 @@ object Test extends App:
   given Migration[E1, E2] = migration
   assert(E1(D1(1), D1(2)).migrateTo[E2] == E2(D2(true, 2), "hi", D2(true, 1)))
 
+  // should only use default when needed
   case class F1(x: Int)
   case class F2(x: Int = 3)
   given Migration[F1, F2] = migration
